f5264bfbef4de4194aec4bd42c25c0fe6524417c
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-02-26 11:46:28 -0300

    feat: flip bits of long and return it

---
+++ week2/FlippingBits.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <bitset>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'flippingBits' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+long flippingBits(long n) {
+    string binary = bitset<32>(n).to_string();
+    cout << binary << endl;
+
+    for (int i = 0; i < 32; i++) {
+        if (binary[i] == '0')
+            binary[i] = '1';
+        else
+            binary[i] = '0';
+    }
+
+    long result = bitset<32>(binary).to_ulong();
+
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        long n = stol(ltrim(rtrim(n_temp)));
+
+        long result = flippingBits(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
